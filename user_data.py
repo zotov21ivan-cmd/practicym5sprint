f28f49fd61a3ef83332264e8fa8e1ab001cadd73
zotov21ivan-cmd <zotov21ivan@gmail.com>
2026-03-28 11:25:33 +0300

    генератор почты и пароля

---
+++ user_data.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_email():
+    number = random.randint(100, 999)
+    return f"Ivan_Zotov_41_{number}@yandex.ru"
+
+def generate_password(length=10):
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
+email = generate_email() 
+password = generate_password()
+
+# для перемещению по сайту во время написания тестов 
+#Ivan_Zotov_41_222@yandex.ru
+# QWEas112As
